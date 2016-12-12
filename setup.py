413d790747fde0926dccd2b6529b21a5674c0c9d
Tobias Koch <tobias.koch@nonterra.com>
2016-12-12 12:23:08 +0300

    Add Python and Debian packaging

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""Bolt OS packaging scripts and tools."""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='bolt-package',
+    version='1.0.0',
+    description='Bolt OS packaging scripts and tools',
+    url='https://github.com/pypa/sampleproject',
+    author='Tobias Koch',
+    author_email='tobias.koch@nonterra.com',
+    license='MIT',
+    package_dir={'': 'lib'},
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+
+    keywords='boltOS packaging development',
+    packages=['com.nonterra.bolt'],
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+)
