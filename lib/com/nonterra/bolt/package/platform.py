c458e6193d7e1acfd752db43602eeb14a176d9ef
Tobias Koch <tobias.koch@nonterra.com>
2017-03-18 14:05:17 +0300

    platform.py: fix arbitrary search in find_executable

--- lib/com/nonterra/bolt/package/platform.py
+++ lib/com/nonterra/bolt/package/platform.py
@@ -72,10 +72,8 @@ class Platform:
 
     @staticmethod
     def find_executable(executable_name):
-        search_path = set(
-            os.environ.get("PATH", "").split(os.pathsep) + \
+        search_path = os.environ.get("PATH", "").split(os.pathsep) + \
                 ["/bin", "/sbin", "/usr/bin", "/usr/sbin"]
-        )
 
         for path in search_path:
             location = os.path.join(path, executable_name)
