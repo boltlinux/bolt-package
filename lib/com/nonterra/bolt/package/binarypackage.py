5f93043e76f3097806625a2043df08c70b65129c
Tobias Koch <tobias.koch@nonterra.com>
2017-05-10 00:30:55 +0300

    fix substring match index range

--- lib/com/nonterra/bolt/package/binarypackage.py
+++ lib/com/nonterra/bolt/package/binarypackage.py
@@ -328,7 +328,8 @@ class BinaryPackage(BasePackage):
         if self.architecture == "tools":
             # filter out /etc and /var directories, these are shared
             def filter_etc_var(item):
-                return False if item[0][0:5] in ["/etc", "/var"] \
+                print(item)
+                return False if item[0][0:4] in ["/etc", "/var"] \
                         else True
             #end inline function
             self.contents = OrderedDict(sorted(filter(filter_etc_var,
