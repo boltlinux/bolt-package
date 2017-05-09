f75b381aa6dffe152564a27864ce69205e41095b
Tobias Koch <tobias.koch@nonterra.com>
2017-05-09 23:08:10 +0300

    fix ref wrong variable in inline function...

--- lib/com/nonterra/bolt/package/binarypackage.py
+++ lib/com/nonterra/bolt/package/binarypackage.py
@@ -328,7 +328,7 @@ class BinaryPackage(BasePackage):
         if self.architecture == "tools":
             # filter out /etc and /var directories, these are shared
             def filter_etc_var(item):
-                return False if path.as_posix()[0:5] in ["/etc", "/var"] \
+                return False if item[0][0:5] in ["/etc", "/var"] \
                         else True
             #end inline function
             self.contents = OrderedDict(sorted(filter(filter_etc_var,
