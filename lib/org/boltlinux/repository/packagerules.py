3e25dfc52cc97c314db3a1c94722c22d24df1d7f
Tobias Koch <tobias.koch@nonterra.com>
2018-06-19 23:42:29 +0200

    repository/packagerules: index package specfiles in DB

--- lib/org/boltlinux/repository/packagerules.py
+++ lib/org/boltlinux/repository/packagerules.py
@@ -30,6 +30,10 @@ import logging
 
 from lxml import etree
 from org.boltlinux.package.appconfig import AppConfig
+from org.boltlinux.package.specfile import Specfile
+from org.boltlinux.repository.flaskapp import app, db
+from org.boltlinux.repository.models import SourcePackage
+from org.boltlinux.error import MalformedSpecfile
 
 class PackageRules:
 
@@ -75,10 +79,67 @@ class PackageRules:
         #end for
     #end function
 
-    def update_repository_db(self, dummy):
-        pass
+    def update_repository_db(self):
+        source_pkg_index = {}
+
+        with app.app_context():
+            for obj in SourcePackage.query.all():
+                source_pkg_index \
+                        .setdefault(obj.name, {}) \
+                        .setdefault(obj.version, obj)
+            #end for
+
+            for repo_info in self.repositories:
+                repo_name  = repo_info["name"]
+                repo_url   = repo_info["repo-url"]
+                repo_rules = repo_info["rules"]
+
+                checkout_dir = self.cache_dir + os.sep + repo_name
+
+                for specfile in self.__next_specfile(checkout_dir):
+                    source_name = specfile.source_name()
+                    version     = specfile.latest_version()
+                    packages    = specfile.binary_packages()
+
+                    xml = etree.tostring(specfile.xml_doc,
+                            pretty_print=True, encoding="unicode")
+
+                    ref_obj = source_pkg_index \
+                            .get(source_name, {})\
+                            .get(version)
+
+                    if ref_obj is None:
+                        source_pkg = SourcePackage(
+                            name    = source_name,
+                            version = version,
+                            xml     = xml
+                        )
+                        db.session.add(source_pkg)
+
+                        source_pkg_index \
+                                .setdefault(source_name, {})\
+                                .setdefault(version, source_pkg)
+                    #end if
+                #end for
+            #end for
+
+            db.session.commit()
+        #end with
+    #end function
 
     # PRIVATE
+
+    def __next_specfile(self, rules_dir):
+        for path, dirs, files in os.walk(rules_dir):
+            if "package.xml" in files:
+                try:
+                    yield Specfile(os.path.join(path, "package.xml"))
+                except MalformedSpecfile as e:
+                    self.log.error("malformed specfile: %s" % str(e))
+                #end try
+            #end if
+        #end for
+    #end function
 
     def __clone_git_repo(self, url, checkout_dir):
         encoding = sys.getdefaultencoding()
