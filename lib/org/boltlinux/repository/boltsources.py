06443b2a98b89888b0065ba02623bccce72f9c19
Tobias Koch <tobias.koch@nonterra.com>
2018-07-16 00:35:22 +0200

    repository/boltsources: trade reliability for a bit of performance

--- lib/org/boltlinux/repository/boltsources.py
+++ lib/org/boltlinux/repository/boltsources.py
@@ -27,6 +27,7 @@ import os
 import sys
 import subprocess
 import logging
+import functools
 
 from lxml import etree
 from org.boltlinux.package.appconfig import AppConfig
@@ -77,18 +78,9 @@ class BoltSources:
     #end function
 
     def update_db(self):
-        source_pkg_index   = {}
-        upstream_src_index = {}
-
         with app.app_context():
-            for obj in SourcePackage.query.all():
-                source_pkg_index \
-                        .setdefault(obj.name, {}) \
-                        .setdefault(obj.version, obj)
-            #end for
-
-            for obj in UpstreamSource.query.all():
-                upstream_src_index[obj.name] = obj
+            source_pkg_index   = self._generate_source_pkg_index()
+            upstream_src_index = self._generate_upstream_src_index()
 
             for repo_info in self._repositories:
                 if self._verbose:
@@ -104,15 +96,69 @@ class BoltSources:
                         upstream_src_index)
             #end for
 
+            # calculate the sortkeys for packages of same name
+            self._sort_source_packages(source_pkg_index)
+
+            # make sure all packages have their upstream ref set
+            self._fix_upstream_refs(source_pkg_index, upstream_src_index)
+
+            # mark packages as recent, up2date, outdated
+            self._determine_recentness(source_pkg_index, upstream_src_index)
+
+            db.session.commit()
+        #end with
+    #end function
+
+    def sort(self):
+        with app.app_context():
+            source_pkg_index = self._generate_source_pkg_index()
+
+            # calculate the sortkeys for packages of same name
+            self._sort_source_packages(source_pkg_index)
+
+            db.session.commit()
+        #end with
+    #end function
+
+    def fix_upstream_refs(self):
+        with app.app_context():
+            source_pkg_index   = self._generate_source_pkg_index()
+            upstream_src_index = self._generate_upstream_src_index()
+
+            self._fix_upstream_refs(source_pkg_index, upstream_src_index)
+
             db.session.commit()
         #end with
     #end function
 
     # PRIVATE
 
+    def _generate_source_pkg_index(self):
+        source_pkg_index = {}
+
+        for obj in SourcePackage.query.all():
+            source_pkg_index \
+                    .setdefault(obj.name, {}) \
+                    .setdefault(obj.version, obj)
+        #end for
+
+        return source_pkg_index
+    #end function
+
+    def _generate_upstream_src_index(self):
+        upstream_src_index = {}
+
+        for obj in UpstreamSource.query.all():
+            upstream_src_index[obj.name] = obj
+
+        return upstream_src_index
+    #end function
+
     def _parse_revisions(self, rules, source_pkg_index, upstream_src_index):
         for revision in rules.revisions():
             revision.checkout()
+
+            commit_id = revision.commit_id[:8]
 
             for package_xml in revision.rules():
                 try:
@@ -127,7 +173,6 @@ class BoltSources:
                 source_name      = specfile.source_name
                 version          = specfile.latest_version
                 upstream_version = specfile.upstream_version
-                packages         = specfile.binary_packages
 
                 xml = etree.tostring(specfile.xml_doc,
                         pretty_print=True, encoding="unicode")
@@ -137,18 +182,23 @@ class BoltSources:
                         .get(version)
 
                 if ref_obj is not None:
-                    self.log.warning(
-                        "Package '%s' at '%.8s' in '%s' modified without version bump." %
-                            (source_name, revision._commit_id, rules._repo_name)
-                    )
-                    ref_obj.xml = xml
-                    ref_obj.upstream_version = upstream_version
+                    if ref_obj.git_hash != commit_id:
+                        self.log.warning(
+                            "Package '%s' at '%.8s' in '%s' modified without version bump." %
+                                (source_name, revision._commit_id, rules._repo_name)
+                        )
+                        ref_obj.xml              = xml
+                        ref_obj.upstream_version = upstream_version
+                        ref_obj.git_hash         = commit_id
+                    #end if
                 else:
                     ref_obj = SourcePackage(
+                        sortkey          = -1,
                         name             = source_name,
                         version          = version,
                         upstream_version = upstream_version,
-                        xml              = xml
+                        git_hash         = commit_id,
+                        xml              = xml,
                     )
                     db.session.add(ref_obj)
 
@@ -162,16 +212,6 @@ class BoltSources:
                 if upstream_version:
                     if upstream_ref_obj:
                         ref_obj.upstream_source_id = upstream_ref_obj.id_
-
-                        comp = BaseXpkg.compare_versions(upstream_version,
-                                upstream_ref_obj.version)
-
-                        if comp < 0:
-                            ref_obj.status = SourcePackage.STATUS_BEHIND
-                        elif comp > 0:
-                            ref_obj.status = SourcePackage.STATUS_AHEAD
-                        else:
-                            ref_obj.status = SourcePackage.STATUS_CURRENT
                     else:
                         self.log.warning(
                             "Package '%s' has no upstream reference." %
@@ -184,6 +224,56 @@ class BoltSources:
                             source_name
                     )
                 #end if
+            #end for
+        #end for
+    #end function
+
+    def _sort_source_packages(self, source_pkg_index):
+        for source_name, entries in source_pkg_index.items():
+            versions = list(entries.keys())
+            versions.sort(key=functools.cmp_to_key(BaseXpkg.compare_versions))
+
+            for i, v in enumerate(versions, start=1):
+                entries[v].sortkey = i
+        #end for
+    #end function
+
+    def _fix_upstream_refs(self, source_pkg_index, upstream_src_index):
+        for source_name, entries in source_pkg_index.items():
+            upstream_ref_obj = upstream_src_index.get(source_name)
+
+            if upstream_ref_obj is None:
+                continue
+
+            for version, ref_obj in entries.items():
+                if ref_obj.upstream_version:
+                    ref_obj.upstream_source_id = upstream_ref_obj.id_
+            #end for
+        #end for
+    #end function
+
+    def _determine_recentness(self, source_pkg_index, upstream_src_index):
+        for source_name, entries in source_pkg_index.items():
+            upstream_ref_obj = upstream_src_index.get(source_name)
+
+            if upstream_ref_obj is None:
+                continue
+
+            for _, ref_obj in entries.items():
+                if not ref_obj.upstream_version:
+                    continue
+
+                comp_result = BaseXpkg.compare_versions(
+                    ref_obj.upstream_version,
+                    upstream_ref_obj.version
+                )
+
+                if comp_result < 0:
+                    ref_obj.status = SourcePackage.STATUS_BEHIND
+                elif comp_result > 0:
+                    ref_obj.status = SourcePackage.STATUS_AHEAD
+                else:
+                    ref_obj.status = SourcePackage.STATUS_CURRENT
             #end for
         #end for
     #end function
