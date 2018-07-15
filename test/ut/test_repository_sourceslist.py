8b57fff7dbd82ed2b050cbccab3176ad3760bc61
Tobias Koch <tobias.koch@nonterra.com>
2018-07-15 16:17:06 +0200

    add first test case

---
+++ test/ut/test_repository_sourceslist.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#-*- encoding: utf-8 -*-
+
+import tempfile
+import os, sys, unittest
+
+SOURCES_DIR = os.path.normpath(os.path.join(
+    os.path.dirname(os.path.realpath(sys.argv[0])),
+    "..", ".."
+))
+
+sys.path.insert(1, SOURCES_DIR + os.sep + 'lib')
+
+from org.boltlinux.repository.sourceslist import SourcesList
+
+class UTTestRepositorySourcesList(unittest.TestCase):
+
+    def test_download_and_iter(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            sources_list = SourcesList(cache_dir=tmpdir)
+
+            sources = list(sources_list)
+            self.assertTrue(len(sources) == 0)
+
+            self.assertFalse(sources_list.is_up2date())
+            sources_list.refresh()
+            self.assertTrue(sources_list.is_up2date())
+
+            sources = list(sources_list)
+            self.assertTrue(len(sources) > 0)
+
+            pkg_info = sources[0]
+
+            for key in ["Package", "Version"]:
+                self.assertTrue(key in pkg_info)
+        #end with
+    #end function
+
+#end class
+
