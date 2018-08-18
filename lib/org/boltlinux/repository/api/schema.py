f97e5021e73275b747e5a2e8a0bfab4c177fad71
Tobias Koch <tobias.koch@gmail.com>
2018-08-18 19:05:04 +0200

    repository/api: implement searching, filtering and pagination

---
+++ lib/org/boltlinux/repository/api/schema.py
@@ -0,0 +1,79 @@
+# -*- encoding: utf-8 -*-
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2018 Tobias Koch <tobias.koch@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import re
+from marshmallow import Schema, fields, validates, post_load, ValidationError
+
+class RequestArgsSchema(Schema):
+    letter = fields.String()
+    search = fields.String()
+    libc   = fields.String()
+    arch   = fields.String()
+    offkey = fields.String()
+    items  = fields.Integer()
+
+    @validates("letter")
+    def validate_letter(self, letter):
+        if ord(letter) < 97 or ord(letter) < 122:
+            raise ValidationError("Letter is out of supported range [a-z]")
+
+    @validates("search")
+    def validate_search(self, search):
+        if re.match(r"^[-a-zA-Z]*$", search) is None:
+            raise ValidationError("Invalid characters in search term.")
+        if len(search) < 2:
+            raise ValidationError("Search term is too short.")
+
+    @validates("libc")
+    def validate_libc(self, libc):
+        if not libc in ["musl", "glibc"]:
+            raise ValidationError("Unknown libc name.")
+
+    @validates("offkey")
+    def validate_offkey(self, offkey):
+        if re.match(r"^[-a-zA-Z]+$", offkey) is None:
+            raise ValidationError("Invalid key offset.")
+
+    @validates("items")
+    def validate_items(self, items):
+        if items < 0 or items > 100:
+            raise ValidationError("Invalid amount of items requested.")
+
+    @post_load
+    def setdefaults(self, data):
+        data.setdefault("libc",  "musl")
+        data.setdefault("arch",  "x86_64")
+        data.setdefault("items", 10)
+
+        for key in ["offkey", "search"]:
+            if key in data:
+                data[key] = data[key].lower()
+        #end for
+
+        return data
+    #end function
+
+#end class
+
--- lib/org/boltlinux/repository/api/sourcepackage.py
+++ lib/org/boltlinux/repository/api/sourcepackage.py
@@ -23,11 +23,14 @@
 # THE SOFTWARE.
 #
 
+from flask import request
 from sqlalchemy.orm import exc as sql_exc
 from werkzeug import exceptions as http_exc
-from flask_restful import Resource, fields, marshal_with, abort
+from flask_restful import Resource, fields, marshal_with
+
 from org.boltlinux.repository.flaskinit import app, db
 from org.boltlinux.repository.models import SourcePackage as SourcePackageModel
+from org.boltlinux.repository.api.schema import RequestArgsSchema
 
 class SourcePackage(Resource):
 
@@ -41,28 +44,48 @@ class SourcePackage(Resource):
     @marshal_with(RESOURCE_FIELDS)
     def get(self, id_=None):
         if id_ is not None:
-            try:
-                return SourcePackageModel.query\
-                    .filter_by(id_=id_)\
-                    .one()
-            except sql_exc.NoResultFound:
-                raise http_exc.NotFound()
+            return self._get_one(id_)
+        else:
+            return self._get_many()
         #end if
+    #end function
 
-        with app.app_context():
-            s1 = db.aliased(SourcePackageModel)
-            s2 = db.aliased(SourcePackageModel)
+    def _get_one(self, id_):
+        try:
+            return SourcePackageModel.query\
+                .filter_by(id_=id_)\
+                .one()
+        except sql_exc.NoResultFound:
+            raise http_exc.NotFound()
+    #end function
 
-            subquery = db.session.query(db.func.max(s1.sortkey))\
-                    .filter_by(name = s2.name)
+    def _get_many(self):
+        req_args, errors = RequestArgsSchema().load(request.args)
+        if errors:
+            raise http_exc.BadRequest(errors)
 
-            query = db.session.query(s2)\
-                    .options(db.defer("xml"))\
-                    .filter_by(sortkey=subquery)\
-                    .order_by(s2.name)
+        offkey = req_args.get("offkey", "")
+        items  = req_args.get("items",  10)
+        search = req_args.get("search", None)
 
-            return query.all()
-        #end with
+        s1 = db.aliased(SourcePackageModel)
+        s2 = db.aliased(SourcePackageModel)
+
+        subquery = db.session.query(db.func.max(s1.sortkey))\
+                .filter_by(name = s2.name)
+
+        query = db.session.query(s2)\
+                .options(db.defer("xml"))\
+                .filter(s2.name > offkey)\
+                .filter_by(sortkey=subquery)\
+                .order_by(s2.name)
+
+        if search:
+            query = query.filter(s2.name.like("%"+search+"%"))
+
+        query = query.limit(items)
+
+        return query.all()
     #end function
 
 #end class
