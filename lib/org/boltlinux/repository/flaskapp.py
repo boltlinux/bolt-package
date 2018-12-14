4c8c1eaccb991c5744b6ba788be8262798691087
Tobias Koch <tobias.koch@gmail.com>
2018-12-14 23:23:20 +0100

    repository/flaskapp: re-enable global search

--- lib/org/boltlinux/repository/flaskapp.py
+++ lib/org/boltlinux/repository/flaskapp.py
@@ -66,12 +66,14 @@ with app.app_context():
 import org.boltlinux.repository.api as api_v1
 
 api.add_resource(api_v1.SourcePackage,
+    "/v1/source",
     "/v1/source/<int:id_>",
     "/v1/source/name/<string:name>",
     "/v1/source/name/<string:name>/version/<string:version>"
 )
 
 api.add_resource(api_v1.BinaryPackage,
+    "/v1/package",
     "/v1/package/<int:id_>",
     "/v1/package/arch/<string:arch>/libc/<string:libc>",
     "/v1/package/arch/<string:arch>/libc/<string:libc>/name/<string:name>",
