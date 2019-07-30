bde03722024f1458dff6f41a1441a9f13b25c941
Tobias Koch <tobias.koch@gmail.com>
2019-07-30 21:12:22 +0200

    environment.sh: prevent sourcing twice

--- environment.sh
+++ environment.sh
@@ -1,7 +1,10 @@
-export FLASK_APP="org.boltlinux.repository.flaskapp:app"
-export FLASK_STATIC_URL_PATH=""
-export FLASK_STATIC_FOLDER="ui/_site"
-export FLASK_ROOT_PATH="."
-export PYTHONPATH="`pwd`/lib"
-export PATH="`pwd`/bin:$PATH"
-export PS1="(bolt-package)$PS1"
+if [ "x$BOLT_PACKAGE_SOURCED" = "x" ]; then
+    export FLASK_APP="org.boltlinux.repository.flaskapp:app"
+    export FLASK_STATIC_URL_PATH=""
+    export FLASK_STATIC_FOLDER="ui/_site"
+    export FLASK_ROOT_PATH="."
+    export PYTHONPATH="`pwd`/lib"
+    export PATH="`pwd`/bin:$PATH"
+    export PS1="(bolt-package)$PS1"
+    export BOLT_PACKAGE_SOURCED="yes"
+fi
