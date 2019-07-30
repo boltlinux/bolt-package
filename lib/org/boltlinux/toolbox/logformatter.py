6fe67f2d18785d1c7868ca1ecb9bd91a1bb8a1e8
Tobias Koch <tobias.koch@gmail.com>
2019-07-30 21:13:15 +0200

    toolbox/logformatter: cleanup and renames

--- lib/org/boltlinux/toolbox/logformatter.py
+++ lib/org/boltlinux/toolbox/logformatter.py
@@ -28,35 +28,34 @@ import logging
 
 class LogFormatter(logging.Formatter):
 
-    HEADER    = '\033[95m'
-    OKBLUE    = '\033[94m'
-    OKGREEN   = '\033[92m'
-    WARNING   = '\033[93m'
-    FAIL      = '\033[91m'
-    ENDC      = '\033[0m'
-    BOLD      = '\033[1m'
-    UNDERLINE = '\033[4m'
+    ESC_BLUE   = '\033[34m'
+    ESC_YELLOW = '\033[33m'
+    ESC_GREEN  = '\033[32m'
+    ESC_RED    = '\033[31m'
+    ESC_BOLD   = '\033[1m'
+    ESC_ENDC   = '\033[0m'
 
     def __init__(self, app_name):
         self._app_name = app_name
 
     def format(self, record):
         if sys.stdout.isatty():
-            template = "{ebold_}{appname}{eendc_}: {ebold_}{elevl_}{levelname}{eendc_}: {message}"
+            template = "{ebold_}{appname}{eendc_}: "\
+                       "{ebold_}{elevl_}{levelname}{eendc_}: {message}"
         else:
             template = "{appname}: {levelname}: {message}"
 
         level_to_eascii = {
-            logging.DEBUG: self.OKBLUE,
-            logging.INFO: self.OKGREEN,
-            logging.WARNING: self.WARNING,
-            logging.ERROR: self.FAIL,
-            logging.CRITICAL: self.FAIL
+            logging.DEBUG: self.ESC_BLUE,
+            logging.INFO: self.ESC_GREEN,
+            logging.WARNING: self.ESC_YELLOW,
+            logging.ERROR: self.ESC_RED,
+            logging.CRITICAL: self.ESC_RED
         }
 
         message = template.format(
-            ebold_=self.BOLD,
-            eendc_=self.ENDC,
+            ebold_=self.ESC_BOLD,
+            eendc_=self.ESC_ENDC,
             elevl_=level_to_eascii[record.levelno],
             appname=self._app_name,
             levelname=record.levelname.lower(),
