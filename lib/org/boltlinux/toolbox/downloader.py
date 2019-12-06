62efe325a7891888c3dec86cdb7afce9365a9cd0
Tobias Koch <tobias.koch@gmail.com>
2019-12-07 00:39:02 +0300

    toolbox/downloader: add tag method for http URLs

--- lib/org/boltlinux/toolbox/downloader.py
+++ lib/org/boltlinux/toolbox/downloader.py
@@ -35,12 +35,13 @@ class Downloader:
     def __init__(self, progress_bar_class=None):
         self._progress_bar_class = progress_bar_class
 
-    def get(self, url, digest=None):
+    def get(self, url, digest=None, connection_timeout=30):
         progress_bar = None
         bytes_read   = 0
 
         try:
-            with urllib.request.urlopen(url) as response:
+            with urllib.request.urlopen(url, timeout=connection_timeout)\
+                    as response:
                 if self._progress_bar_class and response.length:
                     progress_bar = self._progress_bar_class(response.length)
                     progress_bar(0)
@@ -58,7 +59,36 @@ class Downloader:
                 #end for
             #end with
         except urllib.error.URLError as e:
-            raise DownloadError("error retrieving '%s': %s" % (url, str(e)))
+            raise DownloadError(
+                "error retrieving '{}': {}".format(url, str(e))
+            )
+    #end function
+
+    def tag(self, url, connection_timeout=30):
+        alphabet = \
+            string.ascii_uppercase + \
+            string.ascii_lowercase + \
+            string.digits
+
+        try:
+            request = urllib.request.Request(url, method="HEAD")
+            with urllib.request.urlopen(request, timeout=connection_timeout)\
+                    as response:
+                identifier1 = response.getheader("ETag", "")
+                identifier2 = response.getheader(
+                    "Last-Modified",
+                    "".join([random.choice(alphabet) for i in range(16)])
+                )
+        except urllib.error.URLError as e:
+            raise DownloadError(
+                "error generating etag for '{}': {}".format(url, str(e))
+            )
+
+        sha256 = hashlib.sha256()
+        sha256.update(identifier1.encode("utf-8"))
+        sha256.update(identifier2.encode("utf-8"))
+
+        return sha256.hexdigest()[:16]
     #end function
 
 #end class
