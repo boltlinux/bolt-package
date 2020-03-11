41e26e36fcd72e7fda545bc170beda52598c3d99
Tobias Koch <tobias.koch@gmail.com>
2020-03-11 23:01:03 +0300

    deb2bolt: allow disabling security pocket

--- lib/org/boltlinux/deb2bolt/debianpackagecache.py
+++ lib/org/boltlinux/deb2bolt/debianpackagecache.py
@@ -88,6 +88,8 @@ class DebianPackageCache:
             )
         ]
 
+        print(security_enabled, updates_enabled)
+
         if security_enabled:
             self.sources_list.append(
                 (
